5032f054213daad208246a5b650e592dbd8dbb00
Pedreror1 <jorgepedreror1@hotmail.com>
2025-01-06 16:52:00 -0600

    fix ui correct visuals

--- Assets/01.Scripts/XRUIController.cs
+++ Assets/01.Scripts/XRUIController.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class XRUIController : MonoBehaviour
 {
-    public Transform brighthnessUpArrow, brightnessDownArrow;
+    public Transform brightnessUiLines;
     [Range(0f, 100f)]
     public float currentBrightness;
     public GameObject brightnessHolder;
     public TextMeshProUGUI brightnessLabel;
     public float minimumBrightness=50f, maximumBrightness=-50f;
 
-    public Transform audioUpArrow, audioDownArrow;
+    public Transform audioUILines ;
     [Range(0f, 100f)]
     public float currentVolume;
     public GameObject audioHolder;
@@ -40,17 +40,13 @@ public class XRUIController : MonoBehaviour
         brightnessHolder.SetActive(true);
         audioHolder.SetActive(false);
         currentBrightness =newBrightness;
-        Vector3 minPos= brighthnessUpArrow.transform.localPosition;
+        Vector3 minPos= brightnessUiLines.transform.localPosition;
         minPos.x = minimumBrightness;
-        Vector3 maxPos= brighthnessUpArrow.transform.localPosition;
+        Vector3 maxPos= brightnessUiLines.transform.localPosition;
         maxPos.x= maximumBrightness;
-        brighthnessUpArrow.localPosition = Vector3.Lerp(minPos, maxPos, currentBrightness / 100f);
+        brightnessUiLines.localPosition = Vector3.Lerp(minPos, maxPos, currentBrightness / 100f);
 
-         minPos = brightnessDownArrow.transform.localPosition;
-        minPos.x = minimumBrightness;
-         maxPos = brightnessDownArrow.transform.localPosition;
-        maxPos.x = maximumBrightness;
-        brightnessDownArrow.localPosition = Vector3.Lerp(minPos, maxPos, currentBrightness / 100f);
+        
 
         brightnessLabel.text = $"{Mathf.RoundToInt( currentBrightness)}%";
 
@@ -65,17 +61,13 @@ public class XRUIController : MonoBehaviour
 
 
         currentVolume = newVolume;
-        Vector3 minPos = audioUpArrow.transform.localPosition;
+        Vector3 minPos = audioUILines.transform.localPosition;
         minPos.y = minVolume;
-        Vector3 maxPos = audioUpArrow.transform.localPosition;
+        Vector3 maxPos = audioUILines.transform.localPosition;
         maxPos.y = maxVolume;
-        audioUpArrow.localPosition = Vector3.Lerp(minPos, maxPos, currentVolume / 100f);
+        audioUILines.localPosition = Vector3.Lerp(minPos, maxPos, currentVolume / 100f);
 
-        minPos = audioDownArrow.transform.localPosition;
-        minPos.y = minVolume;
-        maxPos = audioDownArrow.transform.localPosition;
-        maxPos.y = maxVolume;
-        audioDownArrow.localPosition = Vector3.Lerp(minPos, maxPos, currentVolume / 100f);
+        
 
         audioLabel.text = $"{Mathf.RoundToInt( currentVolume)}%";
 
